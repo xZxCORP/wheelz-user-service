78e8ea8a9a5fc0ad691358ca18e9ea78dd5a17c9
turpin_l <turpin_l@etna-alternance.net>
2024-07-28 14:11:12 +0200

    feat: adding kysely typing

--- src/config.ts
+++ src/config.ts
@@ -6,6 +6,11 @@ dotenv.config()
 const configSchema = z.object({
   PORT: z.coerce.number().default(3000),
   HOST: z.string().default('localhost'),
+  DB_HOST: z.string().default('localhost'),
+  DB_PORT: z.number().default(3306),
+  DB_USERNAME: z.string().default('wheelz'),
+  DB_PASSWORD: z.string().default('root'),
+  DB_NAME: z.string().default('user_service_db'),
 })
 const parsedConfig = configSchema.safeParse(process.env)
 
