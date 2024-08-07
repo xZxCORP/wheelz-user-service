af831b156b2efffd6ac855feefb167eda3552056
tehcuapm <tehcuapm@gmail.com>
2024-08-07 20:16:01 +0200

    fix: fix type migration

--- src/infrastructure/kysely/migrations/1-create-user-table.ts
+++ src/infrastructure/kysely/migrations/1-create-user-table.ts
@@ -6,9 +6,9 @@ export const up = async (database: Kysely<Database>): Promise<void> => {
   await database.schema
     .createTable('user')
     .addColumn('id', 'serial', (col) => col.primaryKey())
-    .addColumn('firstname', 'text', (col) => col.notNull())
-    .addColumn('lastname', 'text', (col) => col.notNull())
-    .addColumn('email', 'text', (col) => col.notNull().unique())
+    .addColumn('firstname', 'varchar(50)', (col) => col.notNull())
+    .addColumn('lastname', 'varchar(50)', (col) => col.notNull())
+    .addColumn('email', 'varchar(70)', (col) => col.notNull().unique())
     .addColumn('created_at', 'timestamp', (col) => col.defaultTo(sql`now()`).notNull())
     .execute()
 }
