b1509f62ecec8746f8da7263ecaaa3bf137d7cf6
Léo Turpin <turpin_l@etna-alternance.net>
2025-01-23 15:14:51 +0100

    feat: change migration to psql

--- src/infrastructure/kysely/database.ts
+++ src/infrastructure/kysely/database.ts
@@ -1,25 +1,17 @@
 import chalk from 'chalk';
-import { Kysely, MysqlDialect } from 'kysely';
-import mysql2 from 'mysql2';
-import mysql2Promise from 'mysql2/promise';
+import { Kysely, PostgresDialect } from 'kysely';
+import pg from 'pg';
 
 import { config } from '../../config.js';
 import { type Database } from './types.js';
-const pool = mysql2Promise.createPool({
-  host: config.DB_HOST,
-  user: config.DB_USERNAME,
-  password: config.DB_PASSWORD,
-  port: config.DB_PORT,
-});
-const dialect = new MysqlDialect({
-  pool: mysql2.createPool({
+
+const dialect = new PostgresDialect({
+  pool: new pg.Pool({
     database: config.DB_NAME,
     host: config.DB_HOST,
     user: config.DB_USERNAME,
     password: config.DB_PASSWORD,
     port: config.DB_PORT,
-    connectionLimit: 10,
-    maxIdle: 0,
   }),
 });
 
@@ -27,12 +19,22 @@ export const database = new Kysely<Database>({
   dialect,
 });
 export const ensureDatabaseExists = async () => {
-  const connection = await pool.getConnection();
+  const client = new pg.Client({
+    host: config.DB_HOST,
+    user: config.DB_USERNAME,
+    password: config.DB_PASSWORD,
+    port: config.DB_PORT,
+    database: 'postgres',
+  });
+  await client.connect();
   try {
-    const [rows] = await connection.query(`SHOW DATABASES LIKE '${config.DB_NAME}'`);
-    if (Array.isArray(rows) && rows.length === 0) {
+    const { rowCount } = await client.query(
+      'SELECT datname FROM pg_catalog.pg_database WHERE datname = $1',
+      [config.DB_NAME]
+    );
+    if (rowCount === 0) {
       console.log(chalk.yellow(`Database "${config.DB_NAME}" does not exist. Creating...`));
-      await connection.query(`CREATE DATABASE \`${config.DB_NAME}\``);
+      await client.query('CREATE DATABASE $1', [config.DB_NAME]);
       console.log(chalk.green(`Database "${config.DB_NAME}" created successfully.`));
     } else {
       console.log(chalk.blue(`Database "${config.DB_NAME}" already exists.`));
@@ -41,6 +43,6 @@ export const ensureDatabaseExists = async () => {
     console.error(chalk.red('Error checking/creating database:', error));
     throw error;
   } finally {
-    connection.destroy();
+    await client.end();
   }
 };
